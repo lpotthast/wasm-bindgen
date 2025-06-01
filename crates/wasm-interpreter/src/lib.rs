57e0ffd5bf8ae184fc5ec7bf2b87fcafb78fc2fe
Lukas Potthast <privat@lukas-potthast.de>
2025-06-01 16:38:42 +0200

    Increase memory

--- crates/wasm-interpreter/src/lib.rs
+++ crates/wasm-interpreter/src/lib.rs
@@ -71,9 +71,9 @@ impl Interpreter {
 
         // Give ourselves some memory and set the stack pointer
         // (the LLVM call stack, now the Wasm stack, global 0) to the top.
-        // 
+        //
         // Allocates a 5 MiB buffer.
-        ret.mem = vec![0i32; 5 * 1024 * 1024 / size_of::<i32>()];
+        ret.mem = vec![0i32; 100 * 1024 * 1024 / size_of::<i32>()];
         ret.sp = ret.mem.len() as i32;
 
         // Figure out where the `__wbindgen_describe` imported function is, if
