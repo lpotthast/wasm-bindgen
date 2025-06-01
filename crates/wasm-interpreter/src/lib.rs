bee3172ec6214e2051e68b9e2ff6b1948b0fe4c0
Lukas Potthast <privat@lukas-potthast.de>
2025-06-01 17:10:43 +0200

    Update

--- crates/wasm-interpreter/src/lib.rs
+++ crates/wasm-interpreter/src/lib.rs
@@ -311,8 +311,8 @@ impl Frame<'_> {
             Instr::Load(e) => {
                 let address = stack.pop().unwrap();
                 ensure!(
-                    address > 0,
-                    "Load: Read a negative address value from the stack. Did we run out of memory?"
+                    address >= 0,
+                    "Load2: Read a negative address value from the stack. Did we run out of memory?"
                 );
                 let address = address as u32 + e.arg.offset;
                 ensure!(address % 4 == 0);
